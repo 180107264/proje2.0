83a71e85c37ca31f9371c30cd20df5d681806dab
greekbookhero <32012362+greekbookhero@users.noreply.github.com>
2021-03-28 21:48:26 +0600

    Update app.py

--- phase6/app.py
+++ phase6/app.py
@@ -72,16 +72,3 @@ def services():
 
 if __name__ == "__main__":
     app.run(debug=True)
-# con = sqlite3.connect('Proje.db')
-# cur = con.cursor()
-# email = ('d',)
-# password = ('e',)
-# email_from_sql = cur.execute('SELECT email FROM Users WHERE email=?', email).fetchone()
-# password_from_sql = cur.execute('SELECT password FROM Users WHERE password=?', password).fetchone()
-# if email_from_sql and password_from_sql:
-#     print("true")
-# cur.execute('SELECT email FROM Users WHERE email="d"')
-# print(cur.fetchone())
-# var = ('a',)
-# cur.execute('SELECT email FROM Users WHERE email=?', var)
-# print(cur.fetchone())
